aa5f81e3f19dcae8560e9dd7fe0ada9191f3a02c
Andrew Chow <andrewcxw@gmail.com>
2019-02-15 15:43:16 +0000

    Added MockPathfinder and tests

---
+++ Routing/MockPathfinder.py
@@ -0,0 +1,40 @@
+import time
+import random
+
+class Payload(object):
+    def __init__(self):
+        self.drone_routes = []
+    def set_time(self):
+        self.timestamp = int(time.time())
+    def add_drone_route(self, drone_route):
+        self.drone_routes.append(drone_route.__dict__)
+
+class DroneRoute(object):
+    def __init__(self):
+        self.waypoints = []
+    def set_drone_id(self, id):
+        self.drone_id = id
+    def add_waypoint(self, waypoint):
+        self.waypoints.append(waypoint.__dict__)
+
+class Point(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+class MockPathfinder:
+    @staticmethod
+    def getRoutes(data):
+        # create payload
+        payload = Payload()
+        # create route for one drone
+        route = DroneRoute()
+        for i in range(3):
+            point = Point(random.randint(0, 5), 5, random.randint(0, 5))
+            route.add_waypoint(point)
+        route.set_drone_id(1)
+        # add drone route to payload
+        payload.add_drone_route(route)
+        payload.set_time()
+        return payload.__dict__
---
+++ tests/MockPathfinderTest.py
@@ -0,0 +1,52 @@
+import unittest
+import os
+from Routing.MockPathfinder import MockPathfinder
+
+class MockPathfinderTest(unittest.TestCase):
+
+
+    def setUp(self):
+        self.mock_payload = MockPathfinder.getRoutes(0)
+
+
+    def testPayloadFormat(self):
+        self.assertTrue('drone_routes' in self.mock_payload)
+        self.assertTrue('timestamp' in self.mock_payload)
+        self.assertTrue(type(self.mock_payload['drone_routes']) is list)
+
+
+    def testPayloadTypes(self):
+        self.assertIs(type(self.mock_payload), dict)
+        self.assertIs(type(self.mock_payload['timestamp']), int)
+        self.assertIs(type(self.mock_payload['drone_routes']), list)
+
+
+    def testRouteFormat(self):
+        self.mock_route = self.mock_payload['drone_routes'][0]
+        self.assertTrue('waypoints' in self.mock_route)
+        self.assertTrue('drone_id' in self.mock_route)
+
+
+    def testRouteTypes(self):
+        self.mock_route = self.mock_payload['drone_routes'][0]
+        self.assertIs(type(self.mock_route['waypoints']), list)
+        self.assertIs(type(self.mock_route['drone_id']), int)
+
+
+    def testWaypointsFormat(self):
+        self.mock_waypoints = self.mock_payload['drone_routes'][0]['waypoints']
+        self.assertTrue('x' in self.mock_waypoints[0])
+        self.assertTrue('y' in self.mock_waypoints[0])
+        self.assertTrue('z' in self.mock_waypoints[0])
+
+
+    def testWaypointsTypes(self):
+        self.mock_waypoints = self.mock_payload['drone_routes'][0]['waypoints']
+        self.assertIs(type(self.mock_waypoints[0]['x']), int)
+        self.assertIs(type(self.mock_waypoints[0]['y']), int)
+        self.assertIs(type(self.mock_waypoints[0]['z']), int)
+
+
+#    def testFieldTypes(self):
+if __name__ == '__main__':
+    unittest.main()
