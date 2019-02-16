0a8991cb15d2083ed7e34520cfed20a9d0d17b0a
Andrew Chow <andrewcxw@gmail.com>
2019-02-16 20:35:36 +0000

    Added feature to get random coordinates within a radius of lat, lon

--- Scheduling/JobGenerator/JobFactory.py
+++ Scheduling/JobGenerator/JobFactory.py
@@ -1,8 +1,26 @@
 import configparser
+import math
 import random
 import time
 
 from CostFunction import NaiveCostFunction
+
+EARTH_RADIUS = 6371  # km
+# 1 degree latitute in meters
+ONE_DEGREE = EARTH_RADIUS * 2 * math.pi / 360 * 1000
+
+
+def randomPointOnDisk(radius):
+    r = radius * random.uniform(0, 1)
+    theta = random.uniform(0, 1) * 2 * math.pi
+    return (r * math.cos(theta), r * math.sin(theta))
+
+
+def randomCoords(lat, lon, radius):
+    dx, dy = randomPointOnDisk(radius)
+    random_lat = lat + (dy / ONE_DEGREE)
+    random_lon = lon + (dx / (ONE_DEGREE * math.cos(lat * math.pi / 180)))
+    return (random_lat, random_lon)
 
 
 class Job:
@@ -46,6 +64,10 @@ class JobFactory:
         penalty = random.randint(item['penalty'][0], item['penalty'][1])
         valid_for = random.randint(item['valid_for'][0], item['valid_for'][1])
         job.cost_function = NaiveCostFunction(reward, penalty, valid_for)
+        # Assigning pick_up and destination
+        job.pick_up = randomCoords(self.origin[0], self.origin[1], self.range)
+        job.destination = randomCoords(self.origin[0], self.origin[1],
+                                       self.range)
         return job.__dict__
 
     # Ignores the given probability of the job, and picks one at random
