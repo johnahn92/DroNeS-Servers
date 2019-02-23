7dfd034a4effdc9dac5ad04136534a2425ba2939
jsa110 <jsa110@ic.ac.uk>
2019-02-23 18:02:17 +0000

    Formatting updates.

---
+++ tests/test_JobGenerator.py
@@ -0,0 +1,56 @@
+import json
+import os
+import queue
+import unittest
+from jsonschema import validate
+from Scheduling.JobGenerator import JobGenerator, PoissonGenerator
+
+# Mock Args object creator.
+class Args:
+    def __init__(self):
+        self.job_items = [
+            {
+                "item": "mock_item",
+                "reward": [0, 0],
+                "penalty": [0, 0],
+                "valid_for": [0, 0],
+            }
+        ]
+        self.origin = [0, 0]
+        self.radius = 3000
+        self.generator_params = 0.1
+
+
+# JSON schema loader
+def load_schema(filename):
+    file = os.path.join(os.path.dirname(__file__), "schemas/" + filename)
+    with open(file) as f:
+        return json.loads(f.read())
+
+
+class JobGeneratorTest(unittest.TestCase):
+    def setUp(self):
+        self.args = Args()
+        self.running = False
+        self.queue = queue.Queue()
+        self.generator = PoissonGenerator(self.args, self.queue)
+
+    def testStartandStop(self):
+        self.assertFalse(self.generator.running)
+        self.generator.start()
+        self.assertTrue(self.generator.running)
+        self.generator.stop()
+        self.assertFalse(self.generator.running)
+
+    def testAdd_To_Queue(self):
+        schema = load_schema("job.json")
+        self.generator.start()
+        self.generator.stop()
+        job = self.generator.queue.get()
+        # we serialise the cost function object before sending it off
+        job["cost_function"] = job["cost_function"].__dict__
+        validate(job, schema)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_Scheduler.py
@@ -0,0 +1,58 @@
+import json
+import os
+import queue
+import unittest
+from jsonschema import validate
+from Scheduling.Scheduler import JobScheduler, FCFSScheduler
+from MockSimulation.MockSimulation import Simulation
+
+# Mock Args object creator.
+class Args:
+    def __init__(self):
+        self.job_items = [
+            {
+                "item": "mock_item",
+                "reward": [0, 0],
+                "penalty": [0, 0],
+                "valid_for": [0, 0],
+            }
+        ]
+        self.origin = [0, 0]
+        self.radius = 3000
+        self.generator_params = 0.1
+
+
+# JSON schema loader
+def load_schema(filename):
+    file = os.path.join(os.path.dirname(__file__), "schemas/" + filename)
+    with open(file) as f:
+        return json.loads(f.read())
+
+
+class SchedulerTest(unittest.TestCase):
+    def setUp(self):
+        self.args = Args()
+        self.scheduler = FCFSScheduler(self.args)
+        self.mockdata = Simulation().getGeneric()
+
+    def testStartandStop(self):
+        self.assertFalse(self.scheduler.generator.running)
+        self.scheduler.start()
+        self.assertTrue(self.scheduler.generator.running)
+        self.scheduler.stop()
+        self.assertFalse(self.scheduler.generator.running)
+
+    def testGetJob(self):
+        schema = load_schema("job.json")
+        job = self.scheduler.getJob(self.mockdata)
+        self.assertEqual(job, "{}")
+        self.scheduler.start()
+        self.scheduler.stop()
+        job = self.scheduler.getJob(self.mockdata)
+
+        # job is returned as a string and therefore converted to JSON format.
+        validate(json.loads(job), schema)
+
+
+if __name__ == "__main__":
+    unittest.main()
