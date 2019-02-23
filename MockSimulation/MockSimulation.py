6e6cde5621dd525f0da2c37872ccd7a634aecbdc
Andrew Chow <andrewcxw@gmail.com>
2019-02-23 17:27:31 +0000

    Reworked MockSimulation to use JSONEncoder instead

--- MockSimulation/MockSimulation.py
+++ MockSimulation/MockSimulation.py
@@ -1,31 +1,35 @@
-class Jsonable:
-    def json(self):
-        return self.__dict__
+import json
+from json import JSONEncoder
 
 
-class StaticObstacle(Jsonable):
+class Encoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+
+class StaticObstacle:
     def __init__(self, position, size):
-        self.position = Vector3(position).json()
-        self.size = Vector3(size).json()
+        self.position = Vector3(position)
+        self.size = Vector3(size)
 
 
-class Vector3(Jsonable):
+class Vector3:
     def __init__(self, position):
         self.x = position[0]
         self.y = position[1]
         self.z = position[2]
 
 
-class Drone(Jsonable):
+class Drone:
     def __init__(self, id, active, position, pick_up, destination):
         self.id = id
         self.active = active
-        self.position = Vector3(position).json()
-        self.pick_up = Vector3(pick_up).json()
-        self.destination = Vector3(destination).json()
+        self.position = Vector3(position)
+        self.pick_up = Vector3(pick_up)
+        self.destination = Vector3(destination)
 
 
-class Simulation(Jsonable):
+class Simulation:
     def __init__(self):
         self.static_obstacles = []
         self.drones = []
@@ -35,31 +39,31 @@ class Simulation(Jsonable):
         sim = Simulation()
         # add some obstacles
         sim.static_obstacles.append(
-            StaticObstacle([5, 10, 5], [1, 1, 1]).json())
+            StaticObstacle([5, 10, 5], [1, 1, 1]))
         sim.static_obstacles.append(
-            StaticObstacle([10, 10, 10], [1, 1, 1]).json())
+            StaticObstacle([10, 10, 10], [1, 1, 1]))
         sim.static_obstacles.append(
-            StaticObstacle([15, 10, 15], [1, 1, 1]).json())
+            StaticObstacle([15, 10, 15], [1, 1, 1]))
         # add some drones
         sim.drones.append(
-            Drone(1, False, [0, 0, 0], [6, 5, 7], [1, 3, 7]).json())
+            Drone(1, False, [0, 0, 0], [6, 5, 7], [1, 3, 7]))
         sim.drones.append(
-            Drone(2, True, [2, 2, 2], [12, 5, 16], [7, 5, 13]).json())
-        return sim.json()
+            Drone(2, True, [2, 2, 2], [12, 5, 16], [7, 5, 13]))
+        return json.dumps(sim, cls=Encoder)
 
     @staticmethod
     def getNoActiveDrones():
         sim = Simulation()
         # add some obstacles
         sim.static_obstacles.append(
-            StaticObstacle([5, 10, 5], [1, 1, 1]).json())
+            StaticObstacle([5, 10, 5], [1, 1, 1]))
         sim.static_obstacles.append(
-            StaticObstacle([10, 10, 10], [1, 1, 1]).json())
+            StaticObstacle([10, 10, 10], [1, 1, 1]))
         sim.static_obstacles.append(
-            StaticObstacle([15, 10, 15], [1, 1, 1]).json())
+            StaticObstacle([15, 10, 15], [1, 1, 1]))
         # add some drones
         sim.drones.append(
-            Drone(1, False, [0, 0, 0], [6, 5, 7], [1, 3, 7]).json())
+            Drone(1, False, [0, 0, 0], [6, 5, 7], [1, 3, 7]))
         sim.drones.append(
-            Drone(2, False, [2, 2, 2], [12, 5, 16], [7, 5, 13]).json())
-        return sim.json()
+            Drone(2, False, [2, 2, 2], [12, 5, 16], [7, 5, 13]))
+        return json.dumps(sim, cls=Encoder)
